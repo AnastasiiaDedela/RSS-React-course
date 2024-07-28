6008b931a36794c24e19d5b892209cb0fc6cebe4
Anastasiia <nastyadedela@gmail.com>
2024-07-28 20:39:48 +0200

    feat: completed task 3

--- src/main.tsx
+++ src/main.tsx
@@ -5,20 +5,16 @@ import App from './App';
 import ErrorBoundary from '../src/components/ErrorBoundary';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import { ApiProvider } from '@reduxjs/toolkit/query/react';
-import { heroesApi } from './redux/api/heroApi';
 import { ThemeProvider } from './context/ThemeContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      {/* <ApiProvider api={heroesApi}> */}
       <ErrorBoundary>
         <ThemeProvider>
           <App />
         </ThemeProvider>
       </ErrorBoundary>
-      {/* </ApiProvider> */}
     </Provider>
   </React.StrictMode>,
 );
